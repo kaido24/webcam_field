8e5adcf258afcf537917189a4c4eedf73e4a8db3
kaido <kaido@web3.ee>
2018-02-24 01:49:51 +0200

    try to cache

--- src/Controller/WebcamImageController.php
+++ src/Controller/WebcamImageController.php
@@ -13,11 +13,44 @@ class WebcamImageController extends ControllerBase {
     $entityManager = \Drupal::entityTypeManager();
     $entity = $entityManager->getStorage($entity_type)->load($entity_id);
     $item = $entity->get($field_name)->get($delta);
-    $info = pathinfo($item->link);
-    $data  = file_get_contents( $item->link);
-    $imageSrc = 'data:image/'. $info['extension'] . ';base64,'. base64_encode($data);
+    $cid = "{$field_name}_{$entity_type}_{$entity_id}_{$delta}:"  . \Drupal::languageManager()
+        ->getCurrentLanguage()
+        ->getId();
+    $camData = NULL;
+    if ($cache = \Drupal::cache()
+      ->get($cid)) {
+      $camData = $cache->data;
+      $response = new HtmlResponse();
+      $response->setContent($camData['src']);
+
+      if($this->currentTimeMillis() >= $camData['time'] ) {
+        ksm($camData['time']);
+        $info = pathinfo($item->link);
+        $data  = file_get_contents( $item->link);
+        if ($data != false) {
+          $imageSrc = 'data:image/'. $info['extension'] . ';base64,'. base64_encode($data);
+          \Drupal::cache()
+            ->set($cid, ['src' =>  $imageSrc, 'time' => $this->currentTimeMillis() + $item->refresh_rat]);
+        }
+      }
+      return $response;
+    }
+    else {
+      $info = pathinfo($item->link);
+      $data  = file_get_contents( $item->link);
+      if ($data != false) {
+        $imageSrc = 'data:image/' . $info['extension'] . ';base64,' . base64_encode($data);
+        \Drupal::cache()
+          ->set($cid, ['src' => $imageSrc, 'time' => $this->currentTimeMillis() + $item->refresh_rate]);
+      }
+    }
     $response = new HtmlResponse();
-    $response->setContent($imageSrc);
+    $response->setContent($camData['src']);
     return $response;
+  }
+
+  function currentTimeMillis() {
+    list($usec, $sec) = explode(" ", microtime());
+    return round(((float)$usec + (float)$sec) * 1000);
   }
 }
